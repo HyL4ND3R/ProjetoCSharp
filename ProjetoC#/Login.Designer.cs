4bf0ef1e8a344dd222d151ffd1b0548357fa2165
Jean Scholz Konig <jeanbock04.2@hotmail.com>
2026-02-05 18:06:20 -0300

    Add project files.

---
+++ ProjetoC#/Login.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ProjetoC_
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEntrar = new Button();
+            SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(123, 273);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(91, 41);
+            btnEntrar.TabIndex = 0;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.Click += btnEntrar_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(340, 360);
+            Controls.Add(btnEntrar);
+            Name = "Login";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnEntrar;
+    }
+}
---
+++ ProjetoC#/Login.cs
@@ -0,0 +1,15 @@
+namespace ProjetoC_
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
